2730337833ff4c76ab0eb84a219483b77a501319
hideaki tomoda <tomoda1234567890@gmail.com>
2024-04-03 23:37:12 +0900

    競合

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -15,7 +15,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Corporate Site</a>
+            <a class="navbar-brand--bs-primary" href="#">Corporate Site</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -51,6 +51,7 @@
             </div>
         </div>
     </nav>
+    <p class="test">aaaa</p>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
